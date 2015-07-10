17aa3377d9f5ea2d6897af9f895b104c5ad4c874
OlegMoshkovich <omoshkovich@gmail.com>
2015-07-10 16:08:08 +0000

    started footings story

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,9 +1,25 @@
 $( document ).ready(function() {
     console.log( "ready!" );
     
-    $( "#setup-expand" ).click(function() {
+   $( "#setup-expand" ).click(function() {
    $( ".setup-desc" ).slideToggle( "slow");
-
+   if((this).text == "Expand"){
+        $( "#setup-expand" ).text( "Collapse" );
+   }else{
+      $( "#setup-expand" ).text( "Expand" ) ;
+   }
 });
+   
+   $( "#footing-expand" ).click(function() {
+   $( ".footing-desc" ).slideToggle( "slow");
+   if((this).text == "Expand"){
+        $( this ).text( "Collapse" );
+   }else{
+      $( this).text( "Expand" ) ;
+   }
+   
+   
+});
+   
     
 });
